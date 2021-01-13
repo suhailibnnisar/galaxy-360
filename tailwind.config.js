e1a733af09f81e32cd010ae07f42be413abdf4af
Proshashi <proshashi11@gmail.com>
2021-01-13 23:52:18 +0545

    About student and student contact details form screen initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        sm: "24px",
+        md: "32px",
+      },
+      padding: {
+        sm: "24px",
+        md: "32px",
+      },
+      colors: {
+        bgPrim: "#ECECEC",
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
