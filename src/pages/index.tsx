900f4c7d3747d397c9e937c67e64187df3f53bfc
Proshashi <proshashi11@gmail.com>
2021-01-25 01:38:22 +0545

    Redireceted user to signin on dashboard vist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import { useEffect } from 'react';
+import Router from 'next/router';
+
 export default function Home() {
+  useEffect(() => {
+    Router.push('/auth/signin');
+  }, []);
   return <div>Dashboard</div>;
 }
