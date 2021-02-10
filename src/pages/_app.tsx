f8826cf53b9d8a5021fbfcac17efb02c1aa606b6
suhailqubits <suhailqubits@gmail.com>
2021-02-10 23:57:01 +0545

    auth issue fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,14 @@
-import 'tailwindcss/tailwind.css';
-import SidebarStudentDetail from '../components/navigation/SidebarStudentDetail';
-import { useRouter } from 'next/router';
+import "tailwindcss/tailwind.css";
+import SidebarStudentDetail from "../components/navigation/SidebarStudentDetail";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  console.log(router);
 
   return (
-    <div className="h-screen flex overflow-hidden bg-white">
-      {/* <SidebarStudentDetail /> */}
-      <Component {...pageProps} />
-    </div>
+    // <div className="h-screen flex overflow-hidden bg-white">
+    <Component {...pageProps} />
+    // </div>
   );
 }
 
