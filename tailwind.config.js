8d473dd5532ec101bdd6f003e32770d88983498e
suhailqubits <suhailqubits@gmail.com>
2021-02-11 14:27:45 +0545

    production design issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   purge: [
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
