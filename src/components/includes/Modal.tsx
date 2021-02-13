d2342201db8732e14f000e3282055c422a5b142d
Proshashi <proshashi11@gmail.com>
2021-02-13 12:53:57 +0545

    Modal with transition added

--- src/components/includes/Modal.tsx
+++ src/components/includes/Modal.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Transition } from "@tailwindui/react";
 
+import Rodal from "rodal";
+import "rodal/lib/rodal.css";
+
 interface Props {
   visible: boolean;
   setVisible: React.Dispatch<React.SetStateAction<boolean>>;
@@ -10,35 +13,34 @@ const Modal: React.FC<Props> = ({ visible, children, setVisible }) => {
   return (
     <Transition
       show={visible}
-      enter="ease-out duration-300"
+      enter="transition-opacity duration-300"
       enterFrom="opacity-100"
       enterTo="opacity-100"
-      leave="ease-in duration-200"
+      leave="transition-opacity duration-300"
       leaveFrom="opacity-100"
       leaveTo="opacity-0"
     >
-      <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="fixed z-40 inset-0 overflow-y-auto">
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <div
-            className="fixed inset-0 transition-opacity"
+            className="fixed inset-0"
             aria-hidden="true"
             onClick={() => setVisible(false)}
           >
-            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            <div className="absolute inset-0 h-full w-full bg-gray-500 opacity-75 transition-opacity"></div>
           </div>
 
           <Transition
             show={visible}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter="transition-transform ease-out transform duration-300"
+            enterFrom="scale-0"
+            enterTo="scale-100"
+            leave="ease-in transform duration-300"
+            leaveFrom="scale-100"
+            leaveTo="scale-0"
           >
             <div
-              className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle"
-              style={{ zIndex: 99999999 }}
+              className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-transform sm:my-8 sm:align-middle z-40"
               role="dialog"
               aria-modal="true"
               aria-labelledby="modal-headline"
@@ -49,6 +51,28 @@ const Modal: React.FC<Props> = ({ visible, children, setVisible }) => {
         </div>
       </div>
     </Transition>
+  );
+};
+
+export const RodalModal: React.FC<Props> = ({
+  visible,
+  children,
+  setVisible,
+}) => {
+  return (
+    <Rodal
+      visible={visible}
+      animation="slideDown"
+      onClose={() => setVisible(false)}
+      showCloseButton={false}
+      customStyles={{
+        height: "fit-content",
+        width: "fit-content",
+        padding: "0",
+      }}
+    >
+      {children}
+    </Rodal>
   );
 };
 
