41ee6d59b3ec9ad8fdb125fe6b950196922f3161
suhailqubits <suhailqubits@gmail.com>
2021-02-13 12:52:45 +0545

    dropdown focus/blue issue fixed

--- src/components/includes/Select.tsx
+++ src/components/includes/Select.tsx
@@ -40,7 +40,8 @@ const Select: React.FC<Props> = ({
           className={`relative ${
             width || "w-40"
           } bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-pointer focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
-          onClick={() => setToggle((prevState) => !prevState)}
+          onFocus={() => setToggle(!toggle)}
+          onBlur={() => setToggle(false)}
         >
           <span className="block truncate">{defaultValue}</span>
           <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
